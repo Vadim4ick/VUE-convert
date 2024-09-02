c95b97d126cfac51cf315bc4eaacd500b5b227c0
Mac <mac@MacBook-Pro-Mac.local>
2024-09-02 23:28:00 +0400

    fix styles

--- src/components/CoursesForm.vue
+++ src/components/CoursesForm.vue
@@ -29,50 +29,48 @@ const displayRates = computed(() => {
 </script>
 
 <template>
-  <div class="flex flex-col gap-5">
-    <div v-if="currencyStore.isLoading">
-      <SkeletonCourses />
-      <SkeletonCourses />
+  <div v-if="currencyStore.isLoading" class="flex flex-col gap-5">
+    <SkeletonCourses />
+    <SkeletonCourses />
+  </div>
+
+  <div v-if="!currencyStore.isLoading" class="flex flex-col gap-5">
+    <div>
+      <h2 class="text-xl font-semibold mb-2 text-center">Курсы покупки</h2>
+
+      <ul class="space-y-4 flex flex-col justify-center items-center">
+        <li class="text-lg" v-if="currencyStore.currentCurrency !== 'USD'">
+          1 USD = {{ displayRates.toBaseCurrency.usd }}
+          {{ currencyStore.currentCurrency }}
+        </li>
+        <li class="text-lg" v-if="currencyStore.currentCurrency !== 'EUR'">
+          1 EUR = {{ displayRates.toBaseCurrency.eur }}
+          {{ currencyStore.currentCurrency }}
+        </li>
+        <li class="text-lg" v-if="currencyStore.currentCurrency !== 'RUB'">
+          1 RUB = {{ displayRates.fromBaseCurrency.rub }}
+          {{ currencyStore.currentCurrency }}
+        </li>
+      </ul>
     </div>
 
-    <div v-if="!currencyStore.isLoading">
-      <div>
-        <h2 class="text-xl font-semibold mb-2 text-center">Курсы покупки</h2>
+    <div>
+      <h2 class="text-xl font-semibold mb-2 text-center">Курсы продажи</h2>
 
-        <ul class="space-y-4 flex flex-col justify-center items-center">
-          <li class="text-lg" v-if="currencyStore.currentCurrency !== 'USD'">
-            1 USD = {{ displayRates.toBaseCurrency.usd }}
-            {{ currencyStore.currentCurrency }}
-          </li>
-          <li class="text-lg" v-if="currencyStore.currentCurrency !== 'EUR'">
-            1 EUR = {{ displayRates.toBaseCurrency.eur }}
-            {{ currencyStore.currentCurrency }}
-          </li>
-          <li class="text-lg" v-if="currencyStore.currentCurrency !== 'RUB'">
-            1 RUB = {{ displayRates.fromBaseCurrency.rub }}
-            {{ currencyStore.currentCurrency }}
-          </li>
-        </ul>
-      </div>
-
-      <div>
-        <h2 class="text-xl font-semibold mb-2 text-center">Курсы продажи</h2>
-
-        <ul class="space-y-4 flex flex-col justify-center items-center">
-          <li class="text-lg" v-if="currencyStore.currentCurrency !== 'USD'">
-            1 {{ currencyStore.currentCurrency }} =
-            {{ displayRates.fromBaseCurrency.usd }} USD
-          </li>
-          <li class="text-lg" v-if="currencyStore.currentCurrency !== 'EUR'">
-            1 {{ currencyStore.currentCurrency }} =
-            {{ displayRates.fromBaseCurrency.eur }} EUR
-          </li>
-          <li class="text-lg" v-if="currencyStore.currentCurrency !== 'RUB'">
-            1 {{ currencyStore.currentCurrency }} =
-            {{ displayRates.fromBaseCurrency.rub }} RUB
-          </li>
-        </ul>
-      </div>
+      <ul class="space-y-4 flex flex-col justify-center items-center">
+        <li class="text-lg" v-if="currencyStore.currentCurrency !== 'USD'">
+          1 {{ currencyStore.currentCurrency }} =
+          {{ displayRates.fromBaseCurrency.usd }} USD
+        </li>
+        <li class="text-lg" v-if="currencyStore.currentCurrency !== 'EUR'">
+          1 {{ currencyStore.currentCurrency }} =
+          {{ displayRates.fromBaseCurrency.eur }} EUR
+        </li>
+        <li class="text-lg" v-if="currencyStore.currentCurrency !== 'RUB'">
+          1 {{ currencyStore.currentCurrency }} =
+          {{ displayRates.fromBaseCurrency.rub }} RUB
+        </li>
+      </ul>
     </div>
   </div>
 </template>
